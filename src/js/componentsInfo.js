9f472d966da09fd8bb4dc9698dd682a4ec8fc733
Andrei-dot <andreikalachnik@gmail.com>
2023-12-27 13:35:16 +0100

    Added RAM utilization tracking & OS detection wip

--- src/js/componentsInfo.js
+++ src/js/componentsInfo.js
@@ -1,11 +1,49 @@
 const osu                   = require('node-os-utils');
 const { exec }              = require('child_process');
 const { promisify }         = require('util');
-const { copyFileSync } = require('fs');
+const { copyFileSync, cpSync }      = require('fs');
 const execAsync             = promisify(exec);
+const opsys                 = require('os');
+const { stdout, stderr }    = require('process');
+const osvar                 = process.platform;
+//const speedTest             = require('speedtest-net');
 
-const cpu                     = osu.cpu;
-const gpuTempeturyCommand   = 'nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader'; 
+const cpu                       = osu.cpu;
+const gpuTempeturyCommand       = 'nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader'; 
+const gpuNameCmd                = 'nvidia-smi --query-gpu=gpu_name --format=csv, noheader'
+
+const isWin     = false;
+const isMac     = false;
+const isLinux   = false;
+
+addEventListener("DOMContentLoaded", (e) => {
+    switchPlatform();
+});
+
+function switchPlatform() {
+    switch(osvar) {
+        case "win32":
+            // alert("[!] Windows OS"); 
+            isWin = true;
+            break;
+        case "linux":
+            // alert("linux");
+            isLinux = true;
+            break;
+        case "darwin": 
+            // alert("macOs");
+            isMac = true;
+            break;
+        default:
+            alert("[!] Unknown platform.");
+            break;
+    }
+}
+/******************************
+ *  
+ * RAM Information 
+ * 
+ *******************************/
 
 /******************************
  *  
@@ -50,10 +88,28 @@ function editCircleBarTitles(bar,percentage) {
 const loop = setInterval(() => {
     cpu.usage()
     .then(cpuPercentage => {
+        // CPU
         editCircleProgressBarPercentage("circle-progress-bar-cpu",cpuPercentage);
-        editCircleBarTitles("h1-circle-progress-bar",cpuPercentage);
-    })    
-}, 100);
+        editCircleBarTitles("h1-circle-progress-bar",cpuPercentage.toFixed(0));
+    })
+    editCircleProgressBarPercentage("circle-progress-bar-ram",((opsys.freemem / opsys.totalmem) * 100 / 2).toFixed(0));
+    editCircleBarTitles("h1-circle-progress-bar-ram",((opsys.freemem / opsys.totalmem) * 100 / 2).toFixed(0));
+
+    const upload = document.getElementById("up");
+    const download = document.getElementById("download");
+
+    
+    /*exec("speed-test --json", (err, stdout, stderr) => {
+        if(err||stderr) {
+            return upload.innerText = "Up : /", 
+            download.innerText = "Down : /"; 
+        } else {
+            const result = JSON.parse(stdout);
+            upload.innerText = `Up : ${result.upload}`;
+            download.innerText = `Down : ${result.download}`;    
+        }
+    });*/
+}, 500);
 
 const loopGpu = setInterval(() => {
     getGPUTemperature().then(gpuPercentage => {
